701576e005e8a0a97b8fcaa855caa6c899d97a1e
Muhammad Wajahat <wajahatr0@gmail.com>
2024-09-30 22:06:03 +0500

    new

---
+++ app.js
@@ -0,0 +1,30 @@
+// chp 01 
+
+
+// Q 01
+// var abc = "hello to  my web"
+// alert(abc)
+
+// Q 02
+
+// alert("Error! Place enter a valid password")
+
+
+// Q 03
+
+// alert("Error! Place enter a \n valid password");
+
+// Q 04
+
+// alert("Hello");
+// alert("welcome to my web");
+
+// Q 05 
+
+var num1 = "hello i can run javascript through console";
+console.log(num1);
+
+// Q  06
+
+// alert("welcome to my web");
+
