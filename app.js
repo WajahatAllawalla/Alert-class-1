e1e60b7371aec68163e711e73665fb1478796bdb
Muhammad Wajahat <wajahatr0@gmail.com>
2024-09-30 23:44:45 +0500

    ch4-ch5

--- app.js
+++ app.js
@@ -21,8 +21,8 @@
 
 // Q 05 
 
-var num1 = "hello i can run javascript through console";
-console.log(num1);
+// var num1 = "hello i can run javascript through console";
+// console.log(num1);
 
 // Q  06
 
@@ -44,8 +44,8 @@ console.log(num1);
 
  //  Q3
 
- var message="Hello World"
- alert(message);
+//  var message="Hello World"
+//  alert(message);
 
  // Q4
 
@@ -109,3 +109,220 @@ console.log(num1);
 //  var productTitle="Pencil"
 //  var Quaintity = 4
 //  document.write(visitorName+""+"Ordered"+""+Quaintity+""+"pencil on XYZ Store");
+
+
+// CHT 04
+// Q1
+// var userName = "Muhammad Wajahagt"
+// var age = "30"
+// var customer = "wajahat"
+
+// var statement = "<h1>"+ userName +"</h1>"+ "<h1>"+"Age:" +age +"</h1>"+ "<h1>"+ customer +"</h1>"
+
+// document.write(statement)
+
+
+// Q2
+// ligal
+// var my_variable = "wajahat"
+// var $my_variable = "hello"
+// var _my_variable = "usman"
+// var my_variable_example = "talha"
+// var myVariableExample = "wajahat" //need to use this
+// illigal
+// var 1myVariable = "wajahat"
+// var @my_variable = "hello"
+// var ++my_variable = "usman"
+// var %my_variable = "talha"
+// var #my_variable = "wajahat"
+
+
+
+
+// var number = 123
+// var $my_variable = "wajahat"
+// var _my_variable = "talha"
+
+// document.write("<h1>"+number+", "+$my_variable+", "+_my_variable+"</h1>")
+
+// var $my_variable = "$gloves"
+// var _my_variable = "_talha"
+// var name = "name"
+
+// document.write("<h1>"+name+", "+$my_variable+", "+_my_variable+"</h1>")
+
+
+// var nonsensitive = "<s>"+"user Name"+"</s>"
+// var sensitive = "<u>"+"userName"+"</u>"+" for best practice we use this case called camelCase"
+// var statement = "Variable names are case sensitive don't use "+nonsensitive+" use this "+sensitive 
+
+// document.write(statement)
+
+// alert(123)
+// console.log(window.alert)
+// var alert = "<p>"+"JS Keywords"+"</p>"
+
+// document.write(alert)
+// console.log(window.alert)
+
+
+
+
+
+//  MATH EXPRESSIONS
+
+
+
+// Chapter 5
+
+// Q1
+// var num = 3 + 2
+// document.write("sum of 3 and 2 is ="+num)
+
+// Q2
+// var num = 3 + 2
+// var num1 = 3 - 2
+// var num2 = 3 / 2
+// var num3 = 3 * 2
+// document.write(num+", <br>" +num1+", <br>" +num2+", <br>" +num3)
+
+
+// Q3
+// var num = 5
+
+// var num1 = ++num + 7;
+// num1--;
+// var num2 = num1 / 3
+
+// var num3 = 4 % num2
+// console.log(num3)
+// document.write("The remainder is: "+num3)
+
+
+
+// Q4
+// var ticketPrice = 600
+// var buyers = 5
+// var totalCost = 600 * 5
+// console.log(totalCost)
+// var msg = "Total cost to buy 5 tickets to a movie is " +totalCost+ " PKR"
+
+
+// document.write(msg)
+
+
+
+
+
+
+// Q5
+
+
+// for (var i = 1; i<= 10; i++ ) {
+    
+//     document.write("4 x " + i +" = "+ + 4 * i +"<br>")
+// }
+
+// var num = 0
+// var tab = "4 x " + ++num + " = ";
+// var multi = 4 * num
+// document.write(tab + multi +"<br>")
+// var tab1 = "4 x " + ++num + " = ";
+// var multi1 = 4 * num
+// document.write(tab1 + multi1 +"<br>")
+// var tab2 = "4 x " + ++num + " = ";
+// var multi2 = 4 * num
+// document.write(tab2 + multi2 +"<br>")
+// var tab3 = "4 x " + ++num + " = ";
+// var multi3 = 4 * num
+// document.write(tab3 + multi3 +"<br>")
+// var tab4 = "4 x " + ++num + " = ";
+// var multi4 = 4 * num
+// document.write(tab4 + multi4 +"<br>")
+// var tab5 = "4 x " + ++num + " = ";
+// var multi5 = 4 * num
+// document.write(tab5 + multi5 +"<br>")
+// var tab6 = "4 x " + ++num + " = ";
+// var multi6 = 4 * num
+// document.write(tab6 + multi6 +"<br>")
+// var tab7 = "4 x " + ++num + " = ";
+// var multi7 = 4 * num
+// document.write(tab7 + multi7 +"<br>")
+// var tab8 = "4 x " + ++num + " = ";
+// var multi8 = 4 * num
+// document.write(tab8 + multi8 +"<br>")
+// var tab9 = "4 x " + ++num + " = ";
+// var multi9 = 4 * num
+// document.write(tab9 + multi9 +"<br>")
+// var tab10 = "4 x " + ++num + " = ";
+
+
+
+// Q6
+// var celsiusTemp = 25;
+// var fahrenheitTemp = (celsiusTemp * 9/5) + 32;
+// document.write(celsiusTemp+"c is" + fahrenheitTemp+"F"+ "<br>");
+
+// var fahrenheitTemp2 =77;
+// var celsiusTemp2 = (fahrenheitTemp2 - 32) * 5/9;
+// document.write(fahrenheitTemp2+"F is" + celsiusTemp2+"C"+ "<br>");
+
+// Q7
+
+// var item1 = 650
+// var qtyItem1 = 3
+// var item1Total = item1 * qtyItem1
+// var item2 = 100
+// var qtyItem2 = 7
+// var item2Total = item2 * qtyItem2
+// var shippingCharges = 100
+// var subTotal = item1Total + item2Total + shippingCharges
+
+// document.write("<h1>"+"Total cost of your order "+subTotal+"</h1>")
+
+// Q8
+// var totalMarks = 980
+// var marksObtained = 804
+// var percentage = marksObtained / totalMarks *100
+// document.write("<h1>"+"The Percentage of a student is "+percentage+"</h1>")
+
+// Q9
+// var usDollars = 10
+// var saudiRiyals = 25
+// var singleDollar = 104.80
+// var singleRiyal = 28
+// var convertDollars = singleDollar * usDollars
+// var convertRiyal = singleRiyal * saudiRiyals
+// var totalAmountInRupees = convertDollars + convertRiyal
+// document.write("<h1>"+"Total Currency in PKR: "+totalAmountInRupees+"</h1>")
+
+// Q10
+// var num = 5
+// var num = num * 10
+// var num = num / 2
+// document.write("<h1>"+num+"<h1>")
+
+// Q11
+// var currentYear = 2024
+// var birthYear = 1996
+// var myAge = currentYear - birthYear
+// document.write("<h1>"+myAge+"</h1>")
+
+// Q12
+// var pi = 3.142;
+// var radius = 20;
+// var circumference = 2 * pi * radius;
+// console.log(circumference)
+// var area = pi * radius ** 2;
+// console.log(area)
+// document.write("<h1>"+"The circumference is: "+circumference+"</h1>");
+// document.write("<h1>"+"The area is: "+area+"</h1>");
+
+// Q13
+// var favSnacks = "Chocoday"
+// var myAge = 28
+// var featureAge = 60
+// var snacksAmount = 2
+// var totalSnacksNowTillDeath = (featureAge - myAge) * snacksAmount
+
+// document.write("<h1>"+"I'll need "+totalSnacksNowTillDeath+" "+ favSnacks+" to last you until the ripe old age of 60"+"</h1>")
